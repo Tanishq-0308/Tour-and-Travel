2ad002dd209fd8ae90f5d5b727653a7f15a6b685
Tanishq-0308 <123290269+Tanishq-0308@users.noreply.github.com>
2024-05-25 18:40:32 +0530

    update

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ bar.addEventListener("click",()=>{
     else
     navbar.style.right='10px';
 })
+const slide= document.querySelectorAll('.slide');
+const button1=document.querySelector('.button1');
+let counter=0;
+// button1.disabled=true;
+
+slide.forEach(
+    (slide,index)=>{
+        slide.style.left=`${index * 100}%`;
+    }
+)
+
+function previous(){
+    if(counter!=0){
+        // button1.disabled=true;
+        counter--;   
+    slideImage()
+    }
+    console.log(counter);
+    
+}
+function next(){
+    if(counter<slide.length-1){
+        counter++;
+    slideImage()
+    }
+    
+}
+
+const slideImage=()=>{
+    slide.forEach(
+        (slide)=>{
+            slide.style.transform=`translateX(-${counter * 100}%)`
+        }   
+    )
+}
